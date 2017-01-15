e6b9a825be7875898ecebb7f81107b00474efad9
pc <pc@172.16.16.100>
2017-01-15 10:31:04 -0600

    Modificacion

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -6,6 +6,8 @@
 
 package prueba;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author pc
@@ -16,7 +18,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Prueba");
     }
     
 }
